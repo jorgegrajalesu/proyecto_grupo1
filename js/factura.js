a30c5da1fb50d1de0a7e4350ff725ca7fefb9b9f
jorgegrajalesu <jegosystem@gmail.com>
2022-11-21 21:30:10 -0500

    v211122

---
+++ js/factura.js
@@ -0,0 +1,24 @@
+//crear una funcion
+function calcular(){
+    //definir variables iva, precio y cantidad, se realizaran las operaciones
+    const iva = 0.19;
+    let precio = document.simuladorFactura.precio.value;
+    let cantidad = document.simuladorFactura.cantidad.value;
+    producto = document.getElementById('producto').value;
+
+    //validar o aplicar una estructura condicional, para poder hacer el calulo
+    if(precio == 0 || precio == null){
+        alert("No se puede realizar la simulación, intente de nuevo");
+    }else{
+        // realizar las operaciones
+        let valorTotal = precio * cantidad;
+        let valorIva = valorTotal * iva;
+        let totalPagar = valorTotal + valorIva;
+
+        //imprimir los resultados en la pagina con DOM
+        document.getElementById("valorProd").innerHTML = producto;
+        document.getElementById("valorT").innerHTML = valorTotal;
+        document.getElementById("valorI").innerHTML = valorIva;
+        document.getElementById("valorP").innerHTML = totalPagar;
+    }
+}
